4a5b6ff0dfd15f0611a0561c491e2ec5413ec337
Gynthunder <maryadigt@gmail.com>
2023-10-16 22:16:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,12 +12,9 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand navbar-dark bg-dark sticky-top"  data-aos="fade-down">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">PapaLova</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <a class="navbar-brand" href="#">RM Mandi Angin</a>
     <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
       <ul class="navbar-nav">
       <li class="nav-item">
@@ -31,15 +28,58 @@
             Username
           </a>
           <ul class="dropdown-menu dropdown-menu-end mt-2" >
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><a class="dropdown-item" href="#">Setting</a></li>
+            <li><a class="dropdown-item" href="#">Logout</a></li>
           </ul>
         </li>
       </ul>
     </div>
   </div>
 </nav>
+<div class="container-lg">
+  <div class="row">
+    <!-- Sidebar -->
+    <div class="col-lg-3">
+    <nav class="navbar navbar-expand-lg bg-dark rounded-4 border mt-2">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style="width: 250px;">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav nav-tabs flex-column justify-content-end flex-grow-1">
+          <li class="nav-item">
+            <a class="nav-link link-light" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-light" href="#">Order</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-light" href="#">Customer</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-light" href="#">Product</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
+    <!-- End Sidebar -->
+
+    <!-- Content -->
+    <div class="col-lg-9 bg-secondary text-white mt-2">
+
+    </div>
+    <!-- End Content -->
+  </div>
+</div>
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
   <script>
     AOS.init();
