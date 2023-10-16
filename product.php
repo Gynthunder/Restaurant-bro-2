f62476d91cf57e2a454c01f80d43a44e0aadf135
Gynthunder <maryadigt@gmail.com>
2023-10-16 22:48:32 +0700

    index.php

--- product.php
+++ product.php
@@ -47,10 +47,10 @@
     <div class="col-lg-9  text-white mt-2">
     <div class="card">
   <div class="card-header">
-    Home
+    Product
   </div>
   <div class="card-body">
-    <h5 class="card-title">Ini adalah bagian home</h5>
+    <h5 class="card-title">Ini adalah bagian product</h5>
     <p class="card-text">With supporting text below as a natural lead-in to additional content. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam quam repellendus maiores suscipit ullam asperiores, soluta saepe perferendis rerum ducimus quae cupiditate consectetur, minima voluptate mollitia vitae optio, distinctio laborum?</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
---
+++ sidebar.php
@@ -0,0 +1,31 @@
+<div class="col-lg-3 ">
+    <nav class="navbar navbar-expand-lg   rounded-4 border mt-2" style="background-color: #878F70;">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style="width: 250px;">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav nav-tabs flex-column justify-content-end flex-grow-1">
+          <li class="nav-item">
+            <a class="nav-link link-light" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-light" href="order.php">Order</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-light" href="customer.php">Customer</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link link-light" href="product.php">Product</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+    </div>
