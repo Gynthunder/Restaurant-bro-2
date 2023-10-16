db56b0b6a36708c29c8b59ddef3030d9a8894f00
Gynthunder <maryadigt@gmail.com>
2023-10-16 21:14:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">PapaLova</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
